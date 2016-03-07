ad0e36859f6d17a631c9c8d50c8ca8de00ae8527
JXH <917532427@qq.com>
2016-03-07 09:29:07 +0800

    Initial Commit

---
+++ 聊天/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  聊天
+//
+//  Created by mac on 16/3/7.
+//  Copyright © 2016年 JXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 聊天/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  聊天
+//
+//  Created by mac on 16/3/7.
+//  Copyright © 2016年 JXH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
